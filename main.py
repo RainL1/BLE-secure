54407172768c6ee69a91838100526508e903cf22
RainL1 <153647135+RainL1@users.noreply.github.com>
2025-04-19 19:35:29 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class BLE(QWidget):
     def __init__(self):
         super().__init__()
         
-        # Настройка интерфейса
+        
         self.initUI()
 
     def initUI(self):
@@ -19,52 +19,51 @@ class BLE(QWidget):
         self.setGeometry(100, 100, 400, 300)
 
         # Установка фона
-        self.setStyleSheet("background-color:rgb(21, 44, 68);")  # Третий оттенок синего
+        self.setStyleSheet("background-color:rgb(21, 44, 68);")
 
-        # Создание вертикального Layout
         layout = QVBoxLayout()
 
         # Установка шрифта
         font = QFont("Rostic", 12)
         self.setFont(font)
 
-        # Метка статуса с изменением цвета части текста
+        
         self.status_label = QLabel(self)
         self.status_label.setText('<span style="color: white;">Статус соединения: </span>'
-                                  '<span style="color: yellow;">-</span>')  # Изменяем цвет только части текста
-        self.status_label.setStyleSheet("font-size: 30px;")  # Общий стиль для метки
+                                  '<span style="color: yellow;">-</span>')  # Изменение цвета
+        self.status_label.setStyleSheet("font-size: 30px;")  # Размер шрифта
         self.status_label.setAlignment(Qt.AlignCenter)  # Выравнивание по центру
         layout.addWidget(self.status_label)
-
-        # Добавление Spacer для выравнивания
         layout.addItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
 
-        # Кнопка "Начать"
+        # Настройка кнопки "Начать"
         self.start_button = QPushButton("Нажмите, чтобы начать", self)
         self.start_button.setStyleSheet("font-size: 30px; background-color:rgb(3, 132, 174); color: white;")
         self.start_button.clicked.connect(self.start_function)
         layout.addWidget(self.start_button)
 
-        # Устанавливаем Layout
         self.setLayout(layout)
 
     def start_function(self):
-        # Скрыть кнопку
+        # Скрытие кнопки, начало анализа трафика
         self.start_button.hide()
-        subprocess.Popen("sudo tshark -i bluetooth0 -a duration:60 -w pack.pcap", shell=True)
-        time.sleep(60)
-        # Выполнить булеву функцию
+        subprocess.Popen("tshark -i bluetooth0 -a duration:60 -w pack.pcap", shell=True)
+        time.sleep(35)
+        # Получение результата
         result = self.boolean()
-
-        # Обновить статус
-        if result:
+ 
+        # Обновление статуса
+        if result == 2:
             self.status_label.setText('<span style="color: white;">Статус соединения: </span>'
                                        '<span style="color: green;">Безопасно</span>')  # Изменяем цвет текста
-        else:
+        elif result == 1:
             self.status_label.setText('<span style="color: white;">Статус соединения: </span>'
                                        '<span style="color: red;">Небезопасно</span>')  # Изменяем цвет текста
+        else:
+            self.status_label.setText('<span style="color: white;">Статус соединения: </span>'
+                                       '<span style="color: yellow;">Соединения нету/или не BLE</span>')  # Изменяем цвет текста
 
-    def boolean(): # функция, которая анализирует трафик и определяет ключ шифрования, статус соединения
+    def boolean(self): # функция, которая анализирует трафик и определяет ключ шифрования, статус соединения
         connection_status = 0
         security_status = 0
         capture = pyshark.FileCapture('pack.pcap') # переменная, которая хранит трафик открытый через pyshark.FileCapture
@@ -79,12 +78,15 @@ class BLE(QWidget):
             if 'Connect Complete' in str(packet):
                 connection_status = 1
                 break
-        if length == 32: # условие, определяющее безопаснсть соединения
+        
+        
+        if length >= 32: # условие, определяющее безопаснсть соединения
             security_status = 1
         if(connection_status == 1 and security_status == 1):
+            return 2
+        elif connection_status == 1:
             return 1
-        else:
-            return 0
+        else: return 0
 
 
 if __name__ == '__main__':
